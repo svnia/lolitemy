55c536249758ccc1c375fbf2368f79e011a7e659
3111229 <3111229@TE-GDA-D-D37H17>
2022-05-13 13:33:32 +0200

    Dodaj pliki projektów.

---
+++ lol/GiantsBelt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    public class GiantsBelt : Item
+    {
+        public GiantsBelt()
+        {
+            name = "Giant's Belt";
+            price = 900;
+            AddStat(Stat.HP, 350);
+        }
+    }
+}
---
+++ lol/Program.cs
@@ -0,0 +1,25 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+RubyCrystal rb = new RubyCrystal();
+//sprawdz ile ma hp garen
+g.showHP();
+//kup mu item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+
+
+
+
+
+
